7432d6c04818ac4f294e67e85a87026e9e547d70
川本悠人 <eugenenzv8@gmail.com>
2020-08-10 21:57:56 +0900

    add tables

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,19 @@
+class BooksController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body, :user)
+  end
+
+end

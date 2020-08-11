0ce408d512ee887c3310414245430c615283a488
川本悠人 <eugenenzv8@gmail.com>
2020-08-11 12:40:01 +0900

    fix top-routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :books
   # get 'users/show'
   devise_for :users
-  root 'users#show'
+  root 'books#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :edit]
 end

99762ce7e69a5924353e8025734ef0ba6de5b338
川本悠人 <eugenenzv8@gmail.com>
2020-08-11 18:59:22 +0900

    fix user_edit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '8f469fa7d652f5c71f69f967d035d2ad2f4f5162391e571cf3764966858c130f4a42947443eb13f5cd806ee34ea13a186fb39218e6b91b91531a6ab1933cbbae'

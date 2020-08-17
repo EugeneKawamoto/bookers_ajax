92d29b6ea5f97953ed4e612977aee335c1b0385b
川本悠人 <eugenenzv8@gmail.com>
2020-08-17 18:44:35 +0900

    fix user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,9 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-    @book = Book.new
+    unless current_user.id == @user.id
+      redirect_to user_path
+    end
   end
 
   def update

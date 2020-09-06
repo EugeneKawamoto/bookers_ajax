7084b30578102eb4586f207e281692f47911d194
vagrant <vagrant@localhost.localdomain>
2020-09-06 04:50:56 +0000

    add ajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,9 @@
 class FavoritesController < ApplicationController
   def create
     Favorite.create(user_id: current_user.id, book_id: params[:id])
-    redirect_back(fallback_location: books_path)
   end
 
   def destroy
     Favorite.find_by(user_id: current_user.id, book_id: params[:id]).destroy
-    redirect_back(fallback_location: books_path)
   end
 end

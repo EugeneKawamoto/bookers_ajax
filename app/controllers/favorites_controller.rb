23bd1d2fbf40dbb52925fdb7685ad10be0ec50cb
川本悠人 <eugenenzv8@gmail.com>
2020-09-06 16:24:29 +0900

    add favorite_ajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,17 @@
 class FavoritesController < ApplicationController
+  before_action :book_params
+
   def create
     Favorite.create(user_id: current_user.id, book_id: params[:id])
   end
 
   def destroy
     Favorite.find_by(user_id: current_user.id, book_id: params[:id]).destroy
+  end
+
+  private
+
+  def book_params
+    @book = Book.find(params[:id])
   end
 end
